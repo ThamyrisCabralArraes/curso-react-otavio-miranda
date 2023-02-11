9a0fd79b3b733f734b2c4a0be22fa0c6a2601c15
thamyris <thamyriscabral@hotmail.com>
2023-02-11 17:02:54 -0300

    puxei a API

--- src/App.js
+++ src/App.js
@@ -3,47 +3,28 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    count: 0,
-    posts: [
-      {
-        id: 1,
-        name: 'Thamyris cabral',
-        count: 0,
-      },
-      {
-        id: 2,
-        name: 'Thamyris cabral',
-        count: 0,
-      },
-      {
-        id: 3,
-        name: 'Thamyris cabral',
-        count: 0,
-      },
-    ],
+    posts: [],
   };
 
-  componentDidUpdate() {
-    this.componentDidMount();
-  }
+  // componentDidUpdate() {
+  //   this.componentDidMount();
+  // }
 
   componentDidMount() {
-    const { count } = this.state;
-
-    setTimeout(() => {
-      this.setState({ count: count + 1 });
-    }, 1000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((posts) => this.setState({ posts }));
   }
 
   render() {
-    const { posts, count } = this.state;
+    const { posts } = this.state;
     return (
       <div className='App'>
-        <h1>{`Contagem: ${count}`}</h1>
         <div>
           {posts.map((post) => (
             <div key={post.id}>
-              <p>{`Id:  ${post.id} ${post.name}`}</p>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
             </div>
           ))}
         </div>
