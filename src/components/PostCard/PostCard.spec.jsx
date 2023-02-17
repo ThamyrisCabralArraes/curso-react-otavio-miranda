8c550ad9170085f99bf1460722804fe5a22ed316
thamyris <thamyriscabral@hotmail.com>
2023-02-17 10:46:01 -0300

    test: criado tests para o title, cover, body

--- src/components/PostCard/PostCard.spec.jsx
+++ src/components/PostCard/PostCard.spec.jsx
@@ -3,3 +3,13 @@ import { PostCard } from './Index';
 import { postCardMock } from './mock';
 
 const props = postCardMock;
+
+describe('<PostCard />', () => {
+  it('should be render PostCard correctly', () => {
+    render(<PostCard {...props} />);
+
+    expect(screen.getByAltText(/title/i)).toHaveAttribute('src', 'img/img.png');
+    expect(screen.getByRole('heading', { name: /title/i, level: 2 })).toBeInTheDocument();
+    expect(screen.getByText('body1')).toBeInTheDocument();
+  });
+});
