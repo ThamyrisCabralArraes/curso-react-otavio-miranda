4e9f302ea1ce93b844a2f4183dc527a2cca13020
thamyris <thamyriscabral@hotmail.com>
2023-02-17 10:45:08 -0300

    test: criada pasta de tests para o arquivo PostCard

---
+++ src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react';
+import { PostCard } from './Index';
+import { postCardMock } from './mock';
+
+const props = postCardMock;
---
+++ src/components/PostCard/mock.js
@@ -0,0 +1,6 @@
+export const postCardMock = {
+  title: 'title1',
+  id: 1,
+  body: 'body1',
+  cover: 'img/img.png',
+};
